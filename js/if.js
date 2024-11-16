01ee33a73917696e45fc68af2ca7cd4832c4c6e8
Julia Tarasenko <tarasenkojul88@gmail.com>
2024-11-16 13:49:08 -0800

    Create if.js

---
+++ js/if.js
@@ -0,0 +1,13 @@
+/*const userName = Number(prompt(`ведіть число 10`));
+console.log(userName);
+
+if (userName === 10) {
+  console.log(true);
+}
+
+const numA = 5;
+const numB = 2;
+let resalt = numA + numB <= 4 ? 'smol' : 'bige';
+
+console.log(resalt);
+*/
